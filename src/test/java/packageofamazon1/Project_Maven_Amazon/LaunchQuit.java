3a0ff00fc7fbfcd972ed73db90154d089b9302b8
Tarunkumarnagora <nagora.tarun4@gmail.com>
2025-02-02 17:22:57 +0530

    browser update

--- src/test/java/packageofamazon1/Project_Maven_Amazon/LaunchQuit.java
+++ src/test/java/packageofamazon1/Project_Maven_Amazon/LaunchQuit.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,7 +13,7 @@ public class LaunchQuit
   @BeforeMethod	
   public void launch()
   {
-	    driver = new EdgeDriver();
+	    driver = new ChromeDriver();
 		driver.get("https://www.amazon.in");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
